73ffb06d69ed9ff99868b25d66324fc5b187e32f
ak99jpg <anjumkhopekar@gmail.com>
2026-01-15 00:58:30 -0600

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,1 +1,11 @@
-module.exports = {}
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: 'export',
+  images: {
+    unoptimized: true,
+  },
+  // Optional: Add basePath if needed
+  // basePath: '/your-base-path',
+}
+
+module.exports = nextConfig
