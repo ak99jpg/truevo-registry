0b83b0778d0c799953e0b367584f718fb7e15c55
ak99jpg <anjumkhopekar@gmail.com>
2026-01-15 04:39:25 -0600

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,9 +1,13 @@
+/** @type {import('next').NextConfig} */
 const nextConfig = {
- 
+  // No output: 'export'
+  // No output: 'standalone' (let Next.js decide)
   images: {
     unoptimized: true,
   },
+  experimental: {
+    // Optional: Enable if you have issues
+    // outputFileTracingRoot: __dirname,
+  }
 }
-
 module.exports = nextConfig
-
