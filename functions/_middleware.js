8d4595585d30875a141f688d9f7eda531afef8b8
ak99jpg <anjumkhopekar@gmail.com>
2026-01-15 04:38:12 -0600

    Update _middleware.js

--- functions/_middleware.js
+++ functions/_middleware.js
@@ -1,1 +1,12 @@
-
+// functions/_middleware.js
+export async function onRequest(context) {
+  const { next } = context;
+  const response = await next();
+  
+  // Add security headers
+  response.headers.set('X-Frame-Options', 'DENY');
+  response.headers.set('X-Content-Type-Options', 'nosniff');
+  response.headers.set('Referrer-Policy', 'strict-origin-when-cross-origin');
+  
+  return response;
+}
