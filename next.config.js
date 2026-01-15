0fef63090c866d423a6af594598aa605f47be73e
ak99jpg <anjumkhopekar@gmail.com>
2026-01-15 03:26:27 -0600

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,10 +1,9 @@
-// next.config.js
-/** @type {import('next').NextConfig} */
 const nextConfig = {
-  // REMOVED: output: 'export'
+ 
   images: {
     unoptimized: true,
   },
 }
 
 module.exports = nextConfig
+
